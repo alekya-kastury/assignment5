86982c890f27a4610dbf4d6233a4a52d7f2bb8a3
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-12 14:47:29 -0400

    Update A5.py

--- A5.py
+++ A5.py
@@ -13,6 +13,14 @@ import urllib.request
 import whisper
 from pytube import YouTube
 
+from tqdm.auto import tqdm
+from time import sleep
+
+from youtube_transcript_api import YouTubeTranscriptApi
+import openai
+import pinecone
+
+
 st.title("Welcome")
 
 query = st.text_input("How can I assist you today?")
