713189de45f8fc515e24fd08d55961de7c4a55e8
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-12 14:50:01 -0400

    Update A5.py

--- A5.py
+++ A5.py
@@ -24,3 +24,24 @@ import pinecone
 st.title("Welcome")
 
 query = st.text_input("How can I assist you today?")
+youtube_video_url='https://www.youtube.com/watch?v=tmGDx9hVWwo'
+
+youtube=YouTube(youtube_video_url)
+
+duration=youtube.length
+
+yt_title = youtube.title
+
+yt_channel_id=youtube.channel_id
+
+yt_video_id=youtube.video_id
+
+youtube.from_id
+
+streams=youtube.streams.filter(only_audio=True)
+stream=streams.first()
+stream
+
+yt_published_date=youtube.publish_date
+
+stream.download(filename='output.mp4')
