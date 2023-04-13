150bf89f59b4dd4f25da558c6d287aac325be092
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-12 23:13:57 -0400

    Update A5.py

--- A5.py
+++ A5.py
@@ -27,15 +27,6 @@ data = YouTubeTranscriptApi.get_transcript('uFUGJnKByx8')
 data = YouTubeTranscriptApi.get_transcript('D8ft1stIlCY')
 data = YouTubeTranscriptApi.get_transcript('s0CIXvpfv5o')
 
-# check if index already exists (it shouldn't if this is first time)
-if index_name not in pinecone.list_indexes():
-    # if does not exist, create index
-    pinecone.create_index(
-        index_name,
-        dimension=len(res['data'][0]['embedding']),
-        metric='cosine',
-        metadata_config={'indexed': ['channel_id', 'published']}
-    )
 # connect to index
 index = pinecone.Index(index_name)
 # view index stats
