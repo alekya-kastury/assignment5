bd312bbb740fb904e39fa02dc827ab6b9dab0aa4
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-12 20:03:06 -0400

    Update A5.py

--- A5.py
+++ A5.py
@@ -15,25 +15,5 @@ st.title("Welcome")
 
 query = st.text_input("How can I assist you today?")
 youtube_video_url='https://www.youtube.com/watch?v=tmGDx9hVWwo'
-
-youtube=YouTube(youtube_video_url)
-
-duration=youtube.length
-
-yt_title = youtube.title
-
-yt_channel_id=youtube.channel_id
-
-yt_video_id=youtube.video_id
-
-streams=youtube.streams.filter(only_audio=True)
-stream=streams.first()
-stream
-
-yt_published_date=youtube.publish_date
-stream.download(filename='output.mp4')
-model = whisper.load_model(model_name='base')
-
-out=model.transcribe('output_trimmed.mp4')
-
-#out['segments']
+data = YouTubeTranscriptApi.get_transcript('tmGDx9hVWwo')
+st.write(data)
