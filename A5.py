ce5a148d760972415c377d63a4097b257ff2fb38
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-12 22:29:31 -0400

    Update A5.py

--- A5.py
+++ A5.py
@@ -92,9 +92,56 @@ st.title("Welcome")
 
 query = st.text_input("How can I assist you today?")
 
+
+limit = 3750
+
+def retrieve(query):
+    res = openai.Embedding.create(
+        input=[query],
+        engine=embed_model
+    )
+
+    # retrieve from Pinecone
+    xq = res['data'][0]['embedding']
+
+    # get relevant contexts
+    res = index.query(xq, top_k=3, include_metadata=True)
+    contexts = [
+        x['metadata']['text'] for x in res['matches']
+    ]
+
+    # build our prompt with the retrieved contexts included
+    prompt_start = (
+        "Answer the question based on the context below.\n\n"+
+        "Context:\n"
+    )
+    prompt_end = (
+        f"\n\nQuestion: {query}\nAnswer:"
+    )
+    # append contexts until hitting limit
+    for i in range(1, len(contexts)):
+        if len("\n\n---\n\n".join(contexts[:i])) >= limit:
+            prompt = (
+                prompt_start +
+                "\n\n---\n\n".join(contexts[:i-1]) +
+                prompt_end
+            )
+            break
+        elif i == len(contexts)-1:
+            prompt = (
+                prompt_start +
+                "\n\n---\n\n".join(contexts) +
+                prompt_end
+            )
+    return prompt
+
+query_with_contexts = retrieve(query)
+
+st.write(query_with_contexts)
+
 res = openai.Completion.create(
         engine='text-davinci-003',
-        prompt=query,
+        prompt=query_with_contexts,
         temperature=0,
         max_tokens=400,
         top_p=1,
@@ -103,7 +150,5 @@ res = openai.Completion.create(
         stop=None
     )
 
-if query:
-    st.write(res['choice'][0]['text'])
-else:
-    st.write("Please enter a query")
+st.write(res)
+
